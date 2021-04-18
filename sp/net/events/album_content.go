ba5bfc7cf03b4b34beb50d10799475766ca689dc
Tony Pang <tony.pang@unity3d.com>
2021-04-17 20:05:14 -0400

    WIP refactor event handler in SP 9

--- sp/net/events/album_content.go
+++ sp/net/events/album_content.go
@@ -2,108 +2,114 @@ package events
 
 import (
 	"context"
+	"github.com/golang/protobuf/proto"
 	"github.com/stratosnet/sds/framework/spbf"
 	"github.com/stratosnet/sds/msg/header"
 	"github.com/stratosnet/sds/msg/protos"
 	"github.com/stratosnet/sds/sp/net"
 	"github.com/stratosnet/sds/sp/storages/table"
+	"github.com/stratosnet/sds/utils"
 )
 
-// AlbumContent
-type AlbumContent struct {
-	Server *net.Server
+// albumContent is a concrete implementation of event
+type albumContent struct {
+	event
 }
 
-// GetServer
-func (e *AlbumContent) GetServer() *net.Server {
-	return e.Server
+const albumContentEvent = "album_content"
+
+// GetAlbumContentHandler creates event and return handler func for it
+func GetAlbumContentHandler(s *net.Server) EventHandleFunc {
+	return albumContent{newEvent(albumContentEvent, s, albumContentCallbackFunc)}.Handle
+
 }
 
-// SetServer
-func (e *AlbumContent) SetServer(server *net.Server) {
-	e.Server = server
-}
+// albumContentCallbackFunc is the main process of getting album content
+func albumContentCallbackFunc(_ context.Context, s *net.Server, message proto.Message, _ spbf.WriteCloser) (proto.Message, string) {
 
-// Handle
-func (e *AlbumContent) Handle(ctx context.Context, conn spbf.WriteCloser) {
+	body := message.(*protos.ReqAlbumContent)
 
-	target := new(protos.ReqAlbumContent)
+	rsp := &protos.RspAlbumContent{
+		Result: &protos.Result{
+			State: protos.ResultState_RES_SUCCESS,
+		},
+		WalletAddress: body.WalletAddress,
+		ReqId:         body.ReqId,
+		AlbumId:       body.AlbumId,
+	}
 
-	callback := func(message interface{}) (interface{}, string) {
-
-		body := message.(*protos.ReqAlbumContent)
-
-		rsp := &protos.RspAlbumContent{
-			Result: &protos.Result{
-				State: protos.ResultState_RES_SUCCESS,
-			},
-			WalletAddress: body.WalletAddress,
-			ReqId:         body.ReqId,
-			AlbumId:       body.AlbumId,
-		}
-
-		if body.WalletAddress == "" {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "wallet address can't be empty"
-			return rsp, header.RspAlbumContent
-		}
-
-		if body.AlbumId == "" {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "album ID can't be empty"
-			return rsp, header.RspAlbumContent
-		}
-
-		album := new(table.Album)
-		album.AlbumId = body.AlbumId
-
-		if e.GetServer().CT.Fetch(album) != nil {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "album doesn't exist or is deleted"
-			return rsp, header.RspAlbumContent
-		}
-
-		if body.WalletAddress != album.WalletAddress &&
-			album.IsPrivate == table.ALBUM_IS_PRIVATE {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "can't check private album"
-			return rsp, header.RspAlbumContent
-		}
-
-		rsp.OwnerWalletAddress = album.WalletAddress
-		rsp.AlbumInfo = &protos.AlbumInfo{
-			AlbumId:        album.AlbumId,
-			AlbumName:      album.Name,
-			AlbumBlurb:     album.Introduction,
-			AlbumTime:      album.Time,
-			AlbumVisit:     int64(album.VisitCount),
-			AlbumCoverLink: album.GetCoverLink(e.GetServer().Conf.FileStorage.PictureLibAddress),
-			AlbumType:      protos.AlbumType(album.Type),
-			IsPrivate:      album.IsPrivate == table.ALBUM_IS_PRIVATE,
-		}
-
-		userCollectAlbum := new(table.UserCollectAlbum)
-		userCollectAlbum.AlbumId = album.AlbumId
-		userCollectAlbum.WalletAddress = body.WalletAddress
-
-		rsp.IsCollection = false
-		params := map[string]interface{}{
-			"where": map[string]interface{}{
-				"album_id = ? AND wallet_address = ?": []interface{}{album.AlbumId, body.WalletAddress},
-			},
-		}
-		if e.GetServer().CT.FetchTable(userCollectAlbum, params) == nil {
-			rsp.IsCollection = true
-		}
-
-		// 刷新访问次数
-		album.VisitCount = album.VisitCount + 1
-		e.GetServer().CT.Update(album)
-
-		rsp.FileInfo = album.GetFiles(e.GetServer().CT)
-
+	if body.WalletAddress == "" {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "wallet address can't be empty"
 		return rsp, header.RspAlbumContent
 	}
 
-	go net.EventHandle(ctx, conn, target, callback, e.GetServer().Ver)
+	if body.AlbumId == "" {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "album ID can't be empty"
+		return rsp, header.RspAlbumContent
+	}
+
+	album := &table.Album{AlbumId: body.AlbumId}
+
+	if err := s.CT.Fetch(album); err != nil {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "album doesn't exist or is deleted"
+		return rsp, header.RspAlbumContent
+	}
+
+	if body.WalletAddress != album.WalletAddress &&
+		album.IsPrivate == table.ALBUM_IS_PRIVATE {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "can't check private album"
+		return rsp, header.RspAlbumContent
+	}
+
+	rsp.OwnerWalletAddress = album.WalletAddress
+	rsp.AlbumInfo = &protos.AlbumInfo{
+		AlbumId:        album.AlbumId,
+		AlbumName:      album.Name,
+		AlbumBlurb:     album.Introduction,
+		AlbumTime:      album.Time,
+		AlbumVisit:     int64(album.VisitCount),
+		AlbumCoverLink: album.GetCoverLink(s.Conf.FileStorage.PictureLibAddress),
+		AlbumType:      protos.AlbumType(album.Type),
+		IsPrivate:      album.IsPrivate == table.ALBUM_IS_PRIVATE,
+	}
+
+	userCollectAlbum := &table.UserCollectAlbum{
+		AlbumId:       album.AlbumId,
+		WalletAddress: body.WalletAddress,
+	}
+
+	rsp.IsCollection = false
+	params := map[string]interface{}{
+		"where": map[string]interface{}{
+			"album_id = ? AND wallet_address = ?": []interface{}{album.AlbumId, body.WalletAddress},
+		},
+	}
+	if s.CT.FetchTable(userCollectAlbum, params) == nil {
+		rsp.IsCollection = true
+	}
+
+	// update visit count
+	album.VisitCount++
+
+	if err := s.CT.Update(album); err != nil {
+		utils.ErrorLogf(eventHandleErrorTemplate, albumContentEvent, "update album in db", err)
+	}
+
+	rsp.FileInfo = album.GetFiles(s.CT)
+
+	return rsp, header.RspAlbumContent
+}
+
+// Handle create a concrete proto message for this event, and handle the event asynchronously
+func (e *albumContent) Handle(ctx context.Context, conn spbf.WriteCloser) {
+	go func() {
+		target := &protos.ReqAlbumContent{}
+		if err := e.handle(ctx, conn, target); err != nil {
+			utils.ErrorLog(err)
+		}
+	}()
 }
--- sp/net/events/edit_album.go
+++ sp/net/events/edit_album.go
@@ -2,106 +2,106 @@ package events
 
 import (
 	"context"
+	"github.com/golang/protobuf/proto"
 	"github.com/stratosnet/sds/framework/spbf"
 	"github.com/stratosnet/sds/msg/header"
 	"github.com/stratosnet/sds/msg/protos"
 	"github.com/stratosnet/sds/sp/net"
 	"github.com/stratosnet/sds/sp/storages/table"
+	"github.com/stratosnet/sds/utils"
 	"unicode/utf8"
 )
 
-// EditAlbum
-type EditAlbum struct {
-	Server *net.Server
+// editAlbum
+type editAlbum struct {
+	event
 }
 
-// GetServer
-func (e *EditAlbum) GetServer() *net.Server {
-	return e.Server
+const editAlbumEvent = "edit_album"
+
+// GetEditAlbumHandler creates event and return handler func for it
+func GetEditAlbumHandler(s *net.Server) EventHandleFunc {
+	return editAlbum{newEvent(editAlbumEvent, s, editAlbumCallbackFunc)}.Handle
 }
 
-// SetServer
-func (e *EditAlbum) SetServer(server *net.Server) {
-	e.Server = server
-}
+// editAlbumCallbackFunc is the main process of edit album
+func editAlbumCallbackFunc(_ context.Context, s *net.Server, message proto.Message, _ spbf.WriteCloser) (proto.Message, string) {
+	body := message.(*protos.ReqEditAlbum)
 
-// Handle
-func (e *EditAlbum) Handle(ctx context.Context, conn spbf.WriteCloser) {
+	rsp := &protos.RspEditAlbum{
+		Result: &protos.Result{
+			State: protos.ResultState_RES_SUCCESS,
+		},
+		WalletAddress: body.WalletAddress,
+		ReqId:         body.ReqId,
+		AlbumId:       body.AlbumId,
+	}
 
-	target := new(protos.ReqEditAlbum)
-
-	callback := func(message interface{}) (interface{}, string) {
-
-		body := message.(*protos.ReqEditAlbum)
-
-		rsp := &protos.RspEditAlbum{
-			Result: &protos.Result{
-				State: protos.ResultState_RES_SUCCESS,
-			},
-			WalletAddress: body.WalletAddress,
-			ReqId:         body.ReqId,
-			AlbumId:       body.AlbumId,
-		}
-
-		if body.WalletAddress == "" {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "wallet address can't be empty"
-			return rsp, header.RspEditAlbum
-		}
-
-		if body.AlbumId == "" {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "album ID can't be empty"
-			return rsp, header.RspEditAlbum
-		}
-
-		if body.AlbumName == "" {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "album title can't be empty"
-			return rsp, header.RspEditAlbum
-		}
-
-		if utf8.RuneCountInString(body.AlbumName) > 64 {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "album title is too long"
-			return rsp, header.RspEditAlbum
-		}
-
-		album := new(table.Album)
-		album.AlbumId = body.AlbumId
-
-		if e.GetServer().CT.Fetch(album) != nil {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "album doesn't exist"
-			return rsp, header.RspEditAlbum
-		}
-
-		e.GetServer().CT.GetDriver().Delete("album_has_file", map[string]interface{}{"album_id = ?": album.AlbumId})
-
-		if len(body.ChangeFiles) > 0 {
-			for _, file := range body.ChangeFiles {
-				album.AddFile(e.GetServer().CT, file)
-			}
-		}
-
-		var isPrivate byte = table.ALBUM_IS_PUBLIC
-		if body.IsPrivate {
-			isPrivate = table.ALBUM_IS_PRIVATE
-		}
-
-		album.Cover = body.AlbumCoverHash
-		album.Name = body.AlbumName
-		album.Introduction = body.AlbumBlurb
-		album.IsPrivate = isPrivate
-
-		if err := e.GetServer().CT.Update(album); err != nil {
-			rsp.Result.State = protos.ResultState_RES_FAIL
-			rsp.Result.Msg = "failed to edit album " + err.Error()
-			return rsp, header.RspEditAlbum
-		}
-
+	if body.WalletAddress == "" {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "wallet address can't be empty"
 		return rsp, header.RspEditAlbum
 	}
 
-	go net.EventHandle(ctx, conn, target, callback, e.GetServer().Ver)
+	if body.AlbumId == "" {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "album ID can't be empty"
+		return rsp, header.RspEditAlbum
+	}
+
+	if body.AlbumName == "" {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "album title can't be empty"
+		return rsp, header.RspEditAlbum
+	}
+
+	if utf8.RuneCountInString(body.AlbumName) > 64 {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "album title is too long"
+		return rsp, header.RspEditAlbum
+	}
+
+	album := &table.Album{AlbumId: body.AlbumId}
+
+	if s.CT.Fetch(album) != nil {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "album doesn't exist"
+		return rsp, header.RspEditAlbum
+	}
+
+	s.CT.GetDriver().Delete("album_has_file", map[string]interface{}{"album_id = ?": album.AlbumId})
+
+	for _, f := range body.ChangeFiles {
+		if err := album.AddFile(s.CT, f); err != nil {
+			utils.ErrorLogf(eventHandleErrorTemplate, editAlbumEvent, "add changed file to db", err)
+		}
+	}
+
+	var isPrivate byte = table.ALBUM_IS_PUBLIC
+	if body.IsPrivate {
+		isPrivate = table.ALBUM_IS_PRIVATE
+	}
+
+	album.Cover = body.AlbumCoverHash
+	album.Name = body.AlbumName
+	album.Introduction = body.AlbumBlurb
+	album.IsPrivate = isPrivate
+
+	if err := s.CT.Update(album); err != nil {
+		rsp.Result.State = protos.ResultState_RES_FAIL
+		rsp.Result.Msg = "failed to edit album " + err.Error()
+		return rsp, header.RspEditAlbum
+	}
+
+	return rsp, header.RspEditAlbum
+}
+
+// Handle create a concrete proto message for this event, and handle the event asynchronously
+func (e *editAlbum) Handle(ctx context.Context, conn spbf.WriteCloser) {
+	go func() {
+		target := new(protos.ReqEditAlbum)
+		if err := e.handle(ctx, conn, target); err != nil {
+			utils.ErrorLog(err)
+		}
+	}()
 }
