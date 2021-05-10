13cfbb945b76300ab68cdc6eb986e22c0409409f
Marc-Antoine Dumais <marc-antoine@qsnetwork.org>
2021-05-09 23:26:56 -0400

    simplify relayd sds client

--- relay/sds/client.go
+++ relay/sds/client.go
@@ -1,39 +1,29 @@
 package sds
 
 import (
+	"fmt"
 	"github.com/stratosnet/sds/framework/client/cf"
 	"github.com/stratosnet/sds/framework/spbf"
 	"github.com/stratosnet/sds/msg"
-	"github.com/stratosnet/sds/utils"
 	"net"
 )
 
 func NewClient(server string) *cf.ClientConn {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
-	utils.CheckError(err)
-	c, err := net.DialTCP("tcp", nil, tcpAddr)
-	if utils.CheckError(err) {
-		utils.DebugLog(server, "connect failed")
+	if err != nil {
+		fmt.Println("Couldn't resolve TCP address: " + err.Error())
 		return nil
 	}
-	utils.Log("connect success")
-	onConnect := cf.OnConnectOption(func(c spbf.WriteCloser) bool {
-		utils.DebugLog("on connect")
-		return true
-	})
-	onError := cf.OnErrorOption(func(c spbf.WriteCloser) {
-		utils.Log("on error")
-	})
-	onClose := cf.OnCloseOption(func(c spbf.WriteCloser) {
-		utils.Log("on close", c.(*cf.ClientConn).GetName())
-	})
+	c, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Println("DialTCP failed for SDS: " + err.Error())
+		return nil
+	}
+
 	onMessage := cf.OnMessageOption(func(msg msg.RelayMsgBuf, c spbf.WriteCloser) {})
 	bufferSize := cf.BufferSizeOption(100)
 	logOpen := cf.LogOpenOption(true)
 	options := []cf.ClientOption{
-		onConnect,
-		onError,
-		onClose,
 		onMessage,
 		bufferSize,
 		logOpen,
