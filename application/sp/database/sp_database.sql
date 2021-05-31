1efff242fa7d3fb9e8e4b51c99b6cc62691aa047
Marc-Antoine Dumais <marc-antoine@qsnetwork.org>
2021-05-31 16:52:27 -0400

    change active boolean to tinyint

--- application/sp/database/sp_database.sql
+++ application/sp/database/sp_database.sql
@@ -31,7 +31,7 @@ CREATE TABLE pp
     version         int unsigned     NOT NULL DEFAULT '0',
     pub_key         varchar(1000)    NOT NULL DEFAULT '',
     state           tinyint unsigned NOT NULL DEFAULT '0' COMMENT '0:offline,1:online',
-    active          boolean          NOT NULL DEFAULT false,
+    active          tinyint          NOT NULL DEFAULT '0',
     UNIQUE KEY IDX_WALLET_ADDRESS (wallet_address) USING HASH
 ) ENGINE = InnoDB
   DEFAULT CHARSET = UTF8MB4;
