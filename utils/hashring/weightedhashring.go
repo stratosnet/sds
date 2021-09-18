62c0e9752ddf8a7c5a460f9dd599c472587ae7d7
jialbai <maodou_bjl@126.com>
2021-09-17 21:54:27 -0400

    - weightedhashring associated changes

--- utils/hashring/weightedhashring.go
+++ utils/hashring/weightedhashring.go
@@ -11,9 +11,35 @@ func (p WeightedNodeList) Len() int           { return len(p) }
 func (p WeightedNodeList) Less(i, j int) bool { return p[i].OngoingWeight < p[j].OngoingWeight }
 func (p WeightedNodeList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
+type AccumulatedTaskWeight []float64
+
 type WeightedHashRing struct {
 	HashRing       *HashRing
 	NodeTaskWeight map[string]float64 // map(NodeId => 17.00
+	AcmWeight      AccumulatedTaskWeight
+}
+
+func (whr *WeightedHashRing) AddWeightedNode(node *Node) {
+	whr.HashRing.AddNode(node)
+	// TODO get weight
+	whr.NodeTaskWeight[node.ID] = 0.00
+}
+
+func (whr *WeightedHashRing) RemoveWeightedNode(nodeID string) {
+	whr.HashRing.RemoveNode(nodeID)
+	delete(whr.NodeTaskWeight, nodeID)
+}
+
+func (whr *WeightedHashRing) SetOnlineWeighted(nodeID string) {
+	whr.HashRing.SetOnline(nodeID)
+}
+
+func (whr *WeightedHashRing) SetOfflineWeighted(nodeID string) {
+	whr.HashRing.SetOffline(nodeID)
+}
+
+func (whr *WeightedHashRing) RefreshAcmWeights() {
+
 }
 
 func NewWeightedHashRing(numOfVNode uint32) *WeightedHashRing {
