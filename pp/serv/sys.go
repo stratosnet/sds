a6e016fcb0720be6a688152c29a3ee429770e21e
jialbai <maodou_bjl@126.com>
2022-02-07 11:06:32 -0500

    - qb-954: change fmt.print to utils.log

--- pp/serv/sys.go
+++ pp/serv/sys.go
@@ -2,7 +2,6 @@ package serv
 
 import (
 	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/alex023/clock"
@@ -122,34 +121,34 @@ func monitor() {
 	// nv, _ := net.IOCounters(true)
 	// boottime, _ := host.BootTime()
 	// btime := time.Unix(int64(boottime), 0).Format("2006-01-02 15:04:05")
-	fmt.Printf("__________________________________________________________________________\n")
-	fmt.Printf("        Mem         : %v MB  Free: %v MB Used:%v Usage:%f%%\n", v.Total/1024/1024, v.Available/1024/1024, v.Used/1024/1024, v.UsedPercent)
+	utils.Logf("__________________________________________________________________________")
+	utils.Logf("        Mem         : %v MB  Free: %v MB Used:%v Usage:%f%%", v.Total/1024/1024, v.Available/1024/1024, v.Used/1024/1024, v.UsedPercent)
 	if len(c) > 1 {
 		for _, sub_cpu := range c {
 			modelname := sub_cpu.ModelName
 			cores := sub_cpu.Cores
-			fmt.Printf("        CPU          : %v   %v cores \n", modelname, cores)
+			utils.Logf("        CPU          : %v   %v cores ", modelname, cores)
 		}
 	} else {
 		sub_cpu := c[0]
 		modelname := sub_cpu.ModelName
 		cores := sub_cpu.Cores
-		fmt.Printf("        CPU         : %v   %v cores \n", modelname, cores)
+		utils.Logf("        CPU         : %v   %v cores ", modelname, cores)
 
 	}
-	fmt.Printf("        CPU Used    : %f%% \n", cc[0])
-	// fmt.Printf("        Network     : %v bytes / %v bytes\n", nv[0].BytesRxecv, nv[0].BytesSent)
-	// fmt.Printf("        SystemBoot:%v\n", btime)
-	fmt.Printf("        HD          : %v GB  Free: %v GB Usage:%f%%\n", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
-	// fmt.Printf("        OS        : %v(%v)   %v  \n", n.Platform, n.PlatformFamily, n.PlatformVersion)
-	// fmt.Printf("        Hostname  : %v  \n", n.Hostname)
+	utils.Logf("        CPU Used    : %f%% ", cc[0])
+	// utils.Logf("        Network     : %v bytes / %v bytes", nv[0].BytesRxecv, nv[0].BytesSent)
+	// utils.Logf("        SystemBoot:%v", btime)
+	utils.Logf("        HD          : %v GB  Free: %v GB Usage:%f%%", d.Total/1024/1024/1024, d.Free/1024/1024/1024, d.UsedPercent)
+	// utils.Logf("        OS        : %v(%v)   %v  ", n.Platform, n.PlatformFamily, n.PlatformVersion)
+	// utils.Logf("        Hostname  : %v  ", n.Hostname)
 	r := int64(0)
 	w := int64(0)
 	if setting.IsPP && peers.GetPPServer() != nil {
 		r = peers.GetPPServer().Server.GetSecondReadFlow()
 		w = peers.GetPPServer().Server.GetSecondWriteFlow()
-		fmt.Printf("        Upload      : %f MB/s \n", float64(w)/1024/1024)
-		fmt.Printf("        Download    : %f MB/s \n", float64(r)/1024/1024)
+		utils.Logf("        Upload      : %f MB/s ", float64(w)/1024/1024)
+		utils.Logf("        Download    : %f MB/s ", float64(r)/1024/1024)
 	} else if client.PPConn != nil {
 		r = client.PPConn.GetSecondReadFlow()
 		w = client.PPConn.GetSecondWriteFlow()
@@ -161,19 +160,19 @@ func monitor() {
 			w += w1
 			return true
 		})
-		fmt.Printf("        Upload      : %f MB/s \n", float64(w)/1024/1024)
-		fmt.Printf("        Download    : %f MB/s \n", float64(r)/1024/1024)
+		utils.Logf("        Upload      : %f MB/s ", float64(w)/1024/1024)
+		utils.Logf("        Download    : %f MB/s ", float64(r)/1024/1024)
 	} else if client.SPConn != nil {
 		r = client.SPConn.GetSecondReadFlow()
 		w = client.SPConn.GetSecondWriteFlow()
-		fmt.Printf("        Upload      : %f MB/s \n", float64(w)/1024/1024)
-		fmt.Printf("        Download    : %f MB/s \n", float64(r)/1024/1024)
+		utils.Logf("        Upload      : %f MB/s ", float64(w)/1024/1024)
+		utils.Logf("        Download    : %f MB/s ", float64(r)/1024/1024)
 	} else {
-		fmt.Printf("        Upload      : %f MB/s \n", float64(w)/1024/1024)
-		fmt.Printf("        Download    : %f MB/s \n", float64(r)/1024/1024)
+		utils.Logf("        Upload      : %f MB/s ", float64(w)/1024/1024)
+		utils.Logf("        Download    : %f MB/s ", float64(r)/1024/1024)
 	}
 
-	fmt.Printf("__________________________________________________________________________\n")
+	utils.Logf("__________________________________________________________________________")
 
 }
 
