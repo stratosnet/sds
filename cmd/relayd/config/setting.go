4a83198830b1e20b6bfc19c9d38261061ef60475
Marc-Antoine Dumais <marc-antoine@thestratos.org>
2022-05-30 11:11:31 -0400

    feat/QB-1205 Relayd remove framework usage

--- cmd/relayd/config/setting.go
+++ cmd/relayd/config/setting.go
@@ -14,7 +14,6 @@ type sds struct {
 	ClientPort        string            `yaml:"clientPort"`
 	NetworkAddress    string            `yaml:"networkAddress"`
 	WebsocketPort     string            `yaml:"websocketPort"`
-	HandshakePort     string            `yaml:"handshakePort"`
 	ConnectionRetries connectionRetries `yaml:"connectionRetries"`
 }
 
