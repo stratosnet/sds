f9dd54287d4e26fed398becca29d320836142534
qiny <qiny@thestratos.org>
2022-09-09 00:24:54 -0400

    QB-1302 Assign replication task to new joined node before they reach threshold of active

--- pp/requests/requests.go
+++ pp/requests/requests.go
@@ -511,16 +511,14 @@ func ReqTransferDownloadData(notice *protos.ReqFileSliceBackupNotice, newPpP2pAd
 	}
 }
 
-func ReqTransferDownloadWrongData(notice *protos.ReqFileSliceBackupNotice, newPpP2pAddress string) *protos.ReqTransferDownload {
-	return &protos.ReqTransferDownload{
+func ReqTransferDownloadWrongData(notice *protos.ReqFileSliceBackupNotice, newPpP2pAddress string) *protos.ReqTransferDownloadWrong {
+	return &protos.ReqTransferDownloadWrong{
 		TaskId:           notice.TaskId,
 		NewPp:            &protos.PPBaseInfo{P2PAddress: newPpP2pAddress},
 		OriginalPp:       notice.PpInfo,
 		SliceStorageInfo: notice.SliceStorageInfo,
-		SpP2PAddress:     notice.SpP2PAddress,
 		FileHash:         notice.FileHash,
-		SliceNum:         notice.SliceNumber,
-		DeleteOrigin:     notice.DeleteOrigin,
+		Sign:             notice.Sign,
 	}
 }
 
