b94280b2b3b4823dccd0675418b50bd6814c00b0
alexstratos <alex@thestratos.org>
2023-02-24 17:08:40 -0500

    bump version to v0.10.0

--- cmd/relayd/setting/setting.go
+++ cmd/relayd/setting/setting.go
@@ -10,9 +10,9 @@ var Config *config
 var HomePath string
 
 const (
-	VERSION     = "v0.9.0"
-	APP_VER     = 9
-	MIN_APP_VER = 9
+	VERSION     = "v0.10.0"
+	APP_VER     = 10
+	MIN_APP_VER = 10
 )
 
 type AppVersion struct {
