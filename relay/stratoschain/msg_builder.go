21916775e354ee75427319938c07ce851da3f42f
Xixiong Chen <60588630+Xiong-stratos@users.noreply.github.com>
2023-04-24 16:23:57 -0400

    Feat/QB-1773: UpdateStake cmd update (#388)
    * 1, Remove "isIncrStake" parameter from updateStake cmd.
    2, Update code accroding to proto file changes in stratos-chain
    
    * go.mod update

--- relay/stratoschain/msg_builder.go
+++ relay/stratoschain/msg_builder.go
@@ -28,16 +28,16 @@ func BuildVolumeReportMsg(traffic []*Traffic, reporterAddress, reporterOwnerAddr
 		aggregatedVolume[trafficRecord.WalletAddress] += trafficRecord.Volume
 	}
 
-	var nodesVolume []*pottypes.SingleWalletVolume
+	var nodesVolume []pottypes.SingleWalletVolume
 	for walletAddressString, volume := range aggregatedVolume {
 		_, _, err := bech32.DecodeAndConvert(walletAddressString)
 		if err != nil {
 			return nil, []byte{}, err
 		}
 		volume := sdktypes.NewIntFromUint64(volume)
-		nodesVolume = append(nodesVolume, &pottypes.SingleWalletVolume{
+		nodesVolume = append(nodesVolume, pottypes.SingleWalletVolume{
 			WalletAddress: walletAddressString,
-			Volume:        &volume,
+			Volume:        volume,
 		})
 	}
 
@@ -108,7 +108,7 @@ func BuildCreateResourceNodeMsg(nodeType registertypes.NodeType, pubKey []byte, 
 			Amount: stakeAmount.Amount,
 		},
 		ownerAddress[:],
-		&registertypes.Description{
+		registertypes.Description{
 			Moniker: moniker,
 		},
 		uint32(nodeType),
@@ -125,14 +125,14 @@ func BuildCreateMetaNodeMsg(moniker string, pubKey []byte, stakeAmount utiltypes
 			Amount: stakeAmount.Amount,
 		},
 		ownerAddress[:],
-		&registertypes.Description{
+		registertypes.Description{
 			Moniker: moniker,
 		},
 	)
 }
 
 // Stratos-chain 'register' module
-func BuildUpdateResourceNodeStakeMsg(networkAddr, ownerAddr utiltypes.Address, stakeDelta utiltypes.Coin, incrStake bool) sdktypes.Msg {
+func BuildUpdateResourceNodeStakeMsg(networkAddr, ownerAddr utiltypes.Address, stakeDelta utiltypes.Coin) sdktypes.Msg {
 	coin := sdktypes.Coin{
 		Denom:  stakeDelta.Denom,
 		Amount: stakeDelta.Amount,
@@ -141,7 +141,6 @@ func BuildUpdateResourceNodeStakeMsg(networkAddr, ownerAddr utiltypes.Address, s
 		networkAddr[:],
 		ownerAddr[:],
 		coin,
-		incrStake,
 	)
 }
 
