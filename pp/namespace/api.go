d87f87f9702d4f8576c0bb1165e27376c3b0d8be
WangLiStratos <104775735+WangLiStratos@users.noreply.github.com>
2023-06-19 10:31:10 -0400

    fix/QB-1879 fix verify failure while msg to itself (#429)
    * update the index for req-rsp message pair verif
    * fix use of merged global const
    
    ---------
    
    Co-authored-by: alexstratos <58865166+alexstratos@users.noreply.github.com>

--- pp/namespace/api.go
+++ pp/namespace/api.go
@@ -127,7 +127,8 @@ func (api *rpcPubApi) RequestUpload(ctx context.Context, param rpc_api.ParamReqU
 		fileSize := uint64(param.FileSize)
 		signature := param.Signature
 
-		sliceSize := uint64(setting.DefaultSliceBlockSize)
+
+		sliceSize := uint64(setting.MaxSliceSize)
 		sliceCount := uint64(math.Ceil(float64(fileSize) / float64(sliceSize)))
 
 		var slices []*protos.SliceHashAddr
