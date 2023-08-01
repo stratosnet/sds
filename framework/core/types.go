baa7661896b531104d69cfa07539ca4de9e8a720
jialbai <jialiang.bai@thestratos.org>
2023-08-01 15:43:21 -0400

    - qb1992: add comment for const ConnFirstMsgSize

--- framework/core/types.go
+++ framework/core/types.go
@@ -1,6 +1,8 @@
 package core
 
 const (
+	// This is either a client creating a connection, or a temporary connection made for a handshake
+	// Read the first message from the connection. It should indicate what kind of connection it is
 	ConnFirstMsgSize  = 14 // Conn type (8) + server port (2) + channel ID (4)
 	ConnTypeClient    = "client__"
 	ConnTypeHandshake = "handshke"
