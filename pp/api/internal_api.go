51d66b5356ce51d8f35e6ab9711a40ea950d9cfd
qinystratos <qiny@thestratos.org>
2024-05-26 22:28:24 -0400

    feat: add preflight handler to streaming api

--- pp/api/internal_api.go
+++ pp/api/internal_api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/stratosnet/sds/framework/utils/httpserv"
 	"github.com/stratosnet/sds/pp/setting"
@@ -9,11 +10,22 @@ import (
 
 func StartHTTPServ(ctx context.Context) {
 	httpServ := httpserv.MyNewHTTPServ(setting.Config.Streaming.InternalPort)
-	httpServ.MyRoute("/streamVideoStorageInfo/", streamVideoInfoCache)
-	httpServ.MyRoute("/streamSharedVideoStorageInfo/", streamSharedVideoInfoCache)
-	httpServ.MyRoute("/streamVideo/", streamVideoP2P)
+	httpServ.MyRoute("/streamVideoStorageInfo/", corsHandler(streamVideoInfoCache))
+	httpServ.MyRoute("/streamSharedVideoStorageInfo/", corsHandler(streamSharedVideoInfoCache))
+	httpServ.MyRoute("/streamVideo/", corsHandler(streamVideoP2P))
 	httpServ.MyRoute("/streamVideoStorageInfoHttp/", streamVideoInfoHttp)
 	httpServ.MyRoute("/streamVideoHttp/", streamVideoHttp)
 	httpServ.MyRoute("/clearStreamTask/", clearStreamTask)
 	httpServ.MyStart(ctx)
+}
+
+func corsHandler(h func(w http.ResponseWriter, req *http.Request)) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		} else {
+			h(w, r)
+		}
+	}
 }
