0c520af0fcbdb04135600041dbe2c87fbbbf937b
qinystratos <qiny@thestratos.org>
2024-05-26 22:28:24 -0400

    feat: add preflight handler to streaming api

--- pp/api/rest/rest_api.go
+++ pp/api/rest/rest_api.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/stratosnet/sds/framework/utils/httpserv"
 	"github.com/stratosnet/sds/pp/api"
@@ -11,9 +12,20 @@ import (
 func StartHTTPServ(ctx context.Context) {
 	httpServ := httpserv.MyNewHTTPServ(setting.Config.Streaming.RestPort)
 	httpServ.MyRoute("/getOzone/", api.GetOzone)
-	httpServ.MyRoute("/prepareVideoFileCache/", api.PrepareVideoFileCache)
-	httpServ.MyRoute("/prepareSharedVideoFileCache/", api.PrepareSharedVideoFileCache)
-	httpServ.MyRoute("/getVideoSliceCache/", api.GetVideoSliceCache)
-	httpServ.MyRoute("/findVideoSlice/", api.GetVideoSlice)
+	httpServ.MyRoute("/prepareVideoFileCache/", corsHandler(api.PrepareVideoFileCache))
+	httpServ.MyRoute("/prepareSharedVideoFileCache/", corsHandler(api.PrepareSharedVideoFileCache))
+	httpServ.MyRoute("/getVideoSliceCache/", corsHandler(api.GetVideoSliceCache))
+	httpServ.MyRoute("/findVideoSlice/", corsHandler(api.GetVideoSlice))
 	httpServ.MyStart(ctx)
+}
+
+func corsHandler(h func(w http.ResponseWriter, req *http.Request)) func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		} else {
+			h(w, r)
+		}
+	}
 }
