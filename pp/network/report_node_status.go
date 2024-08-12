b87d57ad1e53a923c9f9e4e3b6d1ede1d32d0583
WangLiStratos <wangli@thestratos.org>
2024-08-12 08:25:01 -0400

    change sp when no rsp of node status received

--- pp/network/report_node_status.go
+++ pp/network/report_node_status.go
@@ -2,6 +2,7 @@ package network
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/stratosnet/sds/framework/msg/header"
@@ -10,6 +11,16 @@ import (
 	"github.com/stratosnet/sds/pp/p2pserver"
 	"github.com/stratosnet/sds/pp/requests"
 	"github.com/stratosnet/sds/pp/setting"
+)
+
+var (
+	nodeStatusResponded     = false
+	consecutiveFailureCount = 0
+	nodeStatusMutex         sync.Mutex
+)
+
+const (
+	MAX_NUMBER_NODE_STATUS_FALIURE = 3
 )
 
 // StartStatusReportToSP to start a timer scheduling reporting Node Status to SP
@@ -21,6 +32,18 @@ func (p *Network) StartStatusReportToSP(ctx context.Context) {
 
 func (p *Network) doReportNodeStatus(ctx context.Context) func() {
 	return func() {
+		nodeStatusMutex.Lock()
+		defer nodeStatusMutex.Unlock()
+		if !nodeStatusResponded {
+			if consecutiveFailureCount > MAX_NUMBER_NODE_STATUS_FALIURE {
+				go p.ChangeSp(ctx)
+				return
+			}
+			consecutiveFailureCount++
+		} else {
+			consecutiveFailureCount = 0
+		}
+
 		// scheduled report should only be sent when it's registered
 		if state := p.GetStateFromFsm(); state.Id == STATE_REGISTERED {
 			go p.ReportNodeStatus(ctx)
@@ -33,4 +56,10 @@ func (p *Network) ReportNodeStatus(ctx context.Context) {
 	pp.DebugLog(ctx, "Sending RNS message to SP! "+status.String())
 
 	p2pserver.GetP2pServer(ctx).SendMessageToSPServer(ctx, status, header.ReqReportNodeStatus)
+}
+
+func (p *Network) NodeStatusResponded(ctx context.Context) {
+	nodeStatusMutex.Lock()
+	defer nodeStatusMutex.Unlock()
+	nodeStatusResponded = true
 }
