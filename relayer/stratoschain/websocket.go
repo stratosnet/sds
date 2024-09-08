880ba6e36621cd01ac056c4d594b509607959433
WangLiStratos <wangli@thestratos.org>
2024-09-08 08:55:23 -0400

    add logger for relayd-stchain comm

--- relayer/stratoschain/websocket.go
+++ relayer/stratoschain/websocket.go
@@ -2,7 +2,9 @@ package stratoschain
 
 import (
 	"errors"
+	"os"
 
+	tmlog "github.com/cometbft/cometbft/libs/log"
 	"github.com/cometbft/cometbft/rpc/client/http"
 	"github.com/stratosnet/sds/framework/utils"
 )
@@ -12,11 +14,15 @@ func DialWebsocket(addr string) (*http.HTTP, error) {
 	if err != nil {
 		return nil, err
 	}
+	var Logger tmlog.Logger
+	Logger = tmlog.NewTMLogger(tmlog.NewSyncWriter(os.Stdout))
+	Logger.With("module", "something")
 
 	client, err := http.New(url.String(true, true, false, false), "/websocket")
 	if err != nil {
 		return nil, errors.New("failed to create stratos-chain Client: " + err.Error())
 	}
+	client.SetLogger(Logger)
 	err = client.Start()
 	if err != nil {
 		return nil, errors.New("failed to start stratos-chain Client: " + err.Error())
