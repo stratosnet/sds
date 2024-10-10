a3b075f68efc79040d3d41d2a9491c7d188d4edc
WangLiStratos <wangli@thestratos.org>
2024-10-09 22:22:03 -0400

    fixed consume

--- framework/utils/queue/rabbitMq.go
+++ framework/utils/queue/rabbitMq.go
@@ -13,38 +13,56 @@ const (
 type Queue struct {
 	conn    *amqp.Connection
 	channel *amqp.Channel
+	queue   amqp.Queue
 	msgs    <-chan amqp.Delivery
 }
 
 func NewQueue(url string) *Queue {
 	q := &Queue{}
-	q.conn, _ = amqp.Dial(url)
+	var err error
+	q.conn, err = amqp.Dial(url)
+	if err != nil {
+		return nil
+	}
 	q.channel, _ = q.conn.Channel()
-	err := q.channel.ExchangeDeclare(
-		QUEUE_NAME, // name
-		"topic",    // type
-		true,       // durable
-		false,      // auto-deleted
-		false,      // internal
-		false,      // no-wait
-		nil,        // arguments
+	err = q.channel.ExchangeDeclare(
+		EXCHANGE, // name
+		"topic",  // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
 	)
 	if err != nil {
 
 	}
+	q.queue, _ = q.channel.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
 	return q
 }
 
 func (q *Queue) Subscribe(action string) error {
-	err := q.channel.QueueBind(QUEUE_NAME, action, EXCHANGE, false, nil)
+	err := q.channel.QueueBind(q.queue.Name, action, EXCHANGE, false, nil)
 	if err != nil {
 		return err
 	}
-	q.msgs, err = q.channel.Consume(QUEUE_NAME, action, true, false, false, false, nil)
+
 	return nil
 }
 
 func (q *Queue) GetMsg() <-chan amqp.Delivery {
+	var err error
+	q.msgs, err = q.channel.Consume(q.queue.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return nil
+	}
 	return q.msgs
 }
 
