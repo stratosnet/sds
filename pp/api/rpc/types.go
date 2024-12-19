ae77acbd80f20ff9a20ff5f91d2a7ad3fdd307d2
WangLiStratos <wangli@thestratos.org>
2024-12-19 15:54:38 -0500

    rpc share file operations: unsubsribe while timeout

--- pp/api/rpc/types.go
+++ pp/api/rpc/types.go
@@ -105,8 +105,8 @@ type ParamReqFileList struct {
 type ParamReqShareFile struct {
 	FileHash    string    `json:"filehash"`
 	Signature   Signature `json:"signature"`
-	Duration    int64     `json:"duration"`
-	PrivateFlag bool      `json:"bool"`
+	Duration    int64     `json:"duration,omitempty"`
+	PrivateFlag bool      `json:"private_flag,omitempty"`
 	ReqTime     int64     `json:"req_time"`
 	IpfsCid     string    `json:"ipfs_cid,omitempty"`
 }
