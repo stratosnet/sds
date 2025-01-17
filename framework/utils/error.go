1df820b9d147d465e2645b835ea6891e6973ad11
Marc-Antoine Dumais <marc-antoine@thestratos.org>
2025-01-17 11:32:49 -0500

    fix/ST-762 utils.FormatError support all error types

--- framework/utils/error.go
+++ framework/utils/error.go
@@ -20,7 +20,12 @@ func (e ErrUndefined) Error() string {
 }
 
 type stackTracer interface {
+	Error() string
 	StackTrace() errors.StackTrace
+}
+
+type unwrapper interface {
+	Unwrap() error
 }
 
 func FormatError(err error) string {
@@ -29,15 +34,60 @@ func FormatError(err error) string {
 	}
 
 	str := err.Error()
-	cause := errors.Cause(err)
-	if causeStack, ok := cause.(stackTracer); ok {
-		str += "  "
-		for i, f := range causeStack.StackTrace() {
-			if i != 0 {
-				str += "->"
-			}
-			str += fmt.Sprintf("[%n|%s:%d]", f, f, f)
+
+	var topFrames, trace []errors.Frame
+	var errWithTrace stackTracer
+	var ok bool
+	for {
+		errWithTrace, ok = err.(stackTracer)
+		if !ok {
+			break
+		}
+		trace = errWithTrace.StackTrace()
+		topFrames = append(topFrames, trace[0]) // Save the frame where Wrap is called
+		err = unwrapStackTracer(errWithTrace)
+	}
+
+	if len(topFrames) > 0 {
+		topFrames = topFrames[:len(topFrames)-1] // the last top frame is the first frame from trace
+	}
+	if len(trace) > 2 {
+		trace = trace[:len(trace)-2] // skip what calls our main function (main|proc.go:271 and goexit|asm_amd64.s:1695)
+	}
+	if len(trace) > 5 {
+		trace = trace[:5] // if stack trace is still too long, only keep first 5 locations
+	}
+	trace = append(topFrames, trace...)
+
+	if len(topFrames) > 0 {
+		str += ": wrapped by "
+	}
+	for i, f := range trace {
+		if i == len(topFrames) {
+			str += " caused by "
+		} else if i != 0 {
+			str += "->"
+		}
+		str += fmt.Sprintf("[%n|%v]", f, f)
+	}
+
+	return str
+}
+
+// unwrapStackTracer calls Unwrap on the given stackTracer error until we reach another stackTracer. Returns nil if one can't be found
+func unwrapStackTracer(err stackTracer) error {
+	var curErr error = err
+	for {
+		if curErr == nil {
+			return nil
+		}
+		unwrapperErr, ok := curErr.(unwrapper)
+		if !ok {
+			return nil
+		}
+		curErr = unwrapperErr.Unwrap()
+		if _, ok = curErr.(stackTracer); ok {
+			return curErr
 		}
 	}
-	return str
 }
